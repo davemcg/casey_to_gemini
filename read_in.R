97cd466493168e934add7deb391668fb85374157
DavidMcGaughey <mcgaughey@gmail.com>
2016-02-13 09:39:44 -0500

    fixed function to output the formatted data

--- read_in.R
+++ read_in.R
@@ -6,14 +6,14 @@ casey_files <- list.files()
 
 process_files <- function(file_name){
   data <- read_excel(file_name, skip=4)
-  ID <- strsplit(file_name,"\s")[[1]][1]
-  fname <- strsplit(file_name,"\s")[[1]][2]
-  lname <- strsplit(file_name,"\s")[[1]][3]
+  ID <- strsplit(file_name," ")[[1]][1]
+  fname <- strsplit(file_name," ")[[1]][2]
+  lname <- strsplit(file_name," ")[[1]][3]
   name <- paste(fname, lname)
   data$ID <- ID
   data$name <- name
   output <- data %>% select(ID,name,Gene,Chromosome,HGVSGenomic,HGVSCoding,Panel)
-  return(data)
+  return(output)
   
 }
 
