1377bb6854abd0d8e2edc5f83c2ed2a5243631a1
David McGaughey <mcgaughey@gmail.com>
2016-02-14 20:57:36 -0500

    now creates hgvs for vep

--- xlsx_to_vcf.py
+++ xlsx_to_vcf.py
@@ -1,12 +1,46 @@
 import pandas
+import math
+import sys
+
+"""
+Read in xlsx file, extract hgvs, and ped info
+"""
 
 x = pandas.read_excel("/Users/davidmcgaughey/Desktop/casey/15-3081 Victoria Sang Stargardt v3.2_Report-sign out.xlsx", sheetname=0,skiprows=4)
 
-list(x.columns.values)
-['#ID', 'Gene', 'Chromosome', 'ChromosomePosition', 'ExonNumber', 'HGVSGenomic', 'HGVSCoding', 'HGVSProtein', 'Zygosity', 'Coverage', 'Rs', 'CAF[RA]', 'ExAC_AF', 'Pathogenicity', 'Polyphen2_HDIV_pred', 'Polyphen2_HDIV_score', 'VariantComment', 'TimesObservedPerPanel', 'SamplesPerPanel', 'TimesObservedPerPanelGroup', 'SamplesPerPanelGroup', 'Panel', 'Unnamed: 22', 'Unnamed: 23', 'Unnamed: 24', 'Unnamed: 25']
+#list(x.columns.values)
+#['#ID', 'Gene', 'Chromosome', 'ChromosomePosition', 'ExonNumber', 'HGVSGenomic', 'HGVSCoding', 'HGVSProtein', 'Zygosity', 'Coverage', 'Rs', 'CAF[RA]', 'ExAC_AF', 'Pathogenicity', 'Polyphen2_HDIV_pred', 'Polyphen2_HDIV_score', 'VariantComment', 'TimesObservedPerPanel', 'SamplesPerPanel', 'TimesObservedPerPanelGroup', 'SamplesPerPanelGroup', 'Panel', 'Unnamed: 22', 'Unnamed: 23', 'Unnamed: 24', 'Unnamed: 25']
 
-x['HGVSGenomic']
+#x['HGVSGenomic']
 
-y = x[['Chromosome','HGVSGenomic']]
+#y = x[['Chromosome','HGVSGenomic']]
 
-y['HGVS'] = y["Chromosome"].map(str) + ":" + y["HGVSGenomic"]
+x['HGVS'] = x["Chromosome"].map(str) + ":" + x["HGVSGenomic"]
+
+out = x['HGVS'].tolist()
+
+out2 = [] 
+for object in out:
+	if re.search('^\d+', str(object)):
+		out2.append(object)
+		
+"""
+Run VEP with hgvs, output vcf
+"""
+
+"""
+Merge all vcfs into one vcf
+"""
+
+"""
+Run VEP and annotate for gemini
+"""
+
+"""
+Create gemini db, use ped
+"""
+		
+
+
+
+
