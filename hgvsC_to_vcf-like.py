ebff551ba53db1e033396f538b8e2b673f50a243
davemcg <mcgaughey@gmail.com>
2016-03-01 16:32:12 -0500

    getting closer

--- hgvsC_to_vcf-like.py
+++ hgvsC_to_vcf-like.py
@@ -17,21 +17,15 @@ with open('/Users/mcgaugheyd/GenomicData/hg19.refGene') as infile:
 def get_transcript(name):
 	return transcripts.get(name)
 
-queries = []
-# remove dot notation, if present (e.g. NM_016247.3:c.3381C>T to NM_016247:c.3381C>T)
-for line in open(input):
-	first, second = line.split(':')
-	first = first.split('.')[0]
-	output = first + ':' + second 
-	queries.append(output)
 
 c = open('converter.txt','w')
 f = open('vep_hgvs_input.txt','w')
-for line in queries:
+for line in open(input):
 	chrom, offset, ref, alt = hgvs.parse_hgvs_name(line, genome, get_transcript=get_transcript)
 	chrom = chrom[3:]
 	out = str(chrom) + ' ' + str(offset) + ' . ' + ref + ' ' + alt
 	f.write(out)
+	out2 = line[:-1] + '\t' + str(chrom) + '_' + str(offset) + '_' + ref + '_' + alt
+	c.write(out2)
 f.close()
-
-
+c.close()
