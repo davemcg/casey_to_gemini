5488523fd493d97b93d47a74d8671f539af49c75
David McGaughey <mcgaughey@gmail.com>
2017-06-14 17:05:02 -0400

    new scripts to parse casey xlsx files for hgvs c to convret to vcf

--- casey_xlsx_to_hgvs.py
+++ casey_xlsx_to_hgvs.py
@@ -1,16 +1,13 @@
-#!/usr/bin python3
+#!/usr/local/bin/python3
 
 from openpyxl import load_workbook
 import argparse
-import hgvs.validator
-import hgvs.exceptions
-import hgvs.dataproviders.uta
-import hgvs.parser
-import hgvs.assemblymapper
+import subprocess
+import datetime
 
 parser = argparse.ArgumentParser(description= 'Takes in xlsx files from John Chiang / MVLGenomics Gene Panels and does the following:\n 1. Identifies hidden rows and skips these \n 2. Extracts hgvs \n\n Usage: casey_xlsx_to_hgvs.py YOURFILE.xlsx > output.tsv')
 
-parser.add_argument('xlsx_file', help = 'John Chiang / OSU / MVL patient report xlsx file')
+parser.add_argument('xlsx_file', help = 'John Chiang / OSU / MVL / Casey patient report xlsx file')
 
 args = parser.parse_args()
 
@@ -20,17 +17,52 @@ ws = wb[wb.get_sheet_names()[0]]
 for row in range(2, ws.max_row):
     for column in 'ABCDEFG':
         cell_name = "{}{}".format(column, row)
-        if ws[cell_name].value == 'HGVScoding':
+        if ws[cell_name].value == 'HGVSCoding':
             header_row = row
             HGVS_column = column
         if ws[cell_name].value == 'Zygosity':
             Zygosity_column = column
 
 # get HGVS and zygosity, skipping hidden rows
+hgvs_zygosity = []
 for row in range(header_row+1, ws.max_row):
     if ws.row_dimensions[row].visible is True:
         cell_name_hgvs = "{}{}".format(HGVS_column, row)
         cell_name_zyg = "{}{}".format(Zygosity_column, row)
         if ws[cell_name_hgvs].value is not None and ws[cell_name_hgvs].value[0:2] == 'NM':
-            print(ws[cell_name_hgvs].value, '\t', ws[cell_name_zyg].value)
+            line = [ws[cell_name_hgvs].value, ws[cell_name_zyg].value]
+            hgvs_zygosity.append(line)
+
+# use hgvs_to_vcf.py to convert HGVS to chr, (genomic) coord, ref, alt
+for line in hgvs_zygosity:
+    print(line)
+    vcf = subprocess.check_output(['/Users/mcgaugheyd/git/casey_to_gemini/hgvs_to_vcf.py',line[0]]).decode('utf-8')
+    line.append(vcf)
+    print(line)
+
+
+# write failed conversions to error file
+# and
+# write vcf for gemini annotation
+today = datetime.date.today()
+
+error_file_name = args.xlsx_file.split('.xlsx')[0] + '.error'
+error_file = open(error_file_name, 'w')
+vcf_file_name = args.xlsx_file.split('.xlsx')[0] + '.vcf'
+vcf_file = open(vcf_file_name, 'w')
+vcf_file.write('##fileformat=VCFv4.2\n')
+vcf_file.write('##fileDate=' + str(today).replace('-','') + '\n')
+vcf_file.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSAMPLE\n')
+
+for line in hgvs_zygosity:
+    if line[2].split('\t')[0] == 'ERROR: ':
+        error_file.write('\t'.join(line) + '\n')
+    else:
+        if 'het' in line[1]:
+            output = line[2][:-1] + '\t100\tPASS\t.\tGT:GQ:DP\t' + '0/1:100:100\n'
+            vcf_file.write(output)
+        else:
+            output = line[2][:-1] + '\t100\tPASS\t.\tGT:GQ:DP\t' + '1/1:100:100\n'
+            vcf_file.write(output)
+
 
--- hgvs_to_vcf.py
+++ hgvs_to_vcf.py
@@ -1,27 +1,48 @@
-#!/usr/bin/env python2
+#!/usr/local/bin/python2
 
-import hgvs.parser
-import hgvs.variantmapper
+import hgvs.exceptions
 import hgvs.dataproviders.uta
-
-
-# our variant
-hgvs_c = 'NM_000350.2:c.4774-17_4774-16delGT'
+import hgvs.parser
+import hgvs.assemblymapper
+import sys
 
 # connect to mapping database
-hdp = hgvs.dataproviders.uta.connect()
-evm = hgvs.variantmapper.EasyVariantMapper(hdp,primary_assembly='GRCh37', alt_aln_method='splign')
-
-# parse into hgvs structure
 hp = hgvs.parser.Parser()
-var_c = hp.parse_hgvs_variant(hgvs_c)
-# translate into genomic coordinates
-var_g = evm.c_to_g(var_c)
+hdp = hgvs.dataproviders.uta.connect()
+vm37 = hgvs.assemblymapper.AssemblyMapper(
+    hdp, assembly_name='GRCh37', alt_aln_method='splign')
+
+# unit test inputs
+hgvs_c = 'NM_018474.4:c.226C>T'
 
 
-# start
-var_g.posedit.pos.start.base
-# ref
-var_g.posedit.edit.ref
-# alt
-var_g.posedit.edit.alt
+def converter(hgvs_c):
+	""" Take HGVS (coding) input and return chr, GRCh37 position, reference allele, alternative allele """
+	
+	# parse into hgvs structure
+	var_c = hp.parse_hgvs_variant(hgvs_c)
+	# translate into genomic coordinates
+	try:
+		var_g = vm37.c_to_g(var_c)
+		# chr
+		chr = str(int(var_g.ac.split('.')[0].split('_')[1]))
+		if chr == '23':
+			chr = 'X'
+		if chr == '24':
+			chr = 'Y'
+		# position
+		pos = var_g.posedit.pos.start.base
+		# ref
+		ref = var_g.posedit.edit.ref
+		# alt
+		alt = var_g.posedit.edit.alt
+
+		output = [str(chr), str(pos), '.', str(ref), str(alt)]
+		return output
+	except Exception as e:
+		output = ["ERROR: ", str(e)]
+		return(output)
+
+if __name__ == '__main__':
+	vcf_like = converter(sys.argv[1])
+	print('\t'.join(vcf_like))
