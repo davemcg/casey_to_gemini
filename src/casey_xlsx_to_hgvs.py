376401fd32f6efd391d3d5927d1a18d2f3866480
David McGaughey <mcgaughey@gmail.com>
2017-07-30 07:06:40 -0400

    now handles cases where VEP not needed

--- src/casey_xlsx_to_hgvs.py
+++ src/casey_xlsx_to_hgvs.py
@@ -37,10 +37,10 @@ for row in range(header_row+1, ws.max_row):
 try:
     for row in range(header_row+1, ws.max_row):
         if ws[Panel_column + str(row)].value is not None:
-            panel_type = ws[Panel_column + str(row)].value.replace(' ','_')
+            panel_type = ws[Panel_column + str(row)].value.replace(' ','_').replace('(','_').replace(')','_')
 except:
     panel_type = '_'.join(args.xlsx_file.split(' ')[3:])
-    panel_type = panel_type.replace('.xlsx','')
+    panel_type = panel_type.replace('.xlsx','').replace('(','_').replace(')','_')
 
 # fill in value for transcript_column if missing (only OLD 2015 panels have this field)
 try:
@@ -76,18 +76,18 @@ for row in range(header_row+1, last_row):
                 hgvs_zygosity[ws[cell_name_hgvs].value] = ws[cell_name_zyg].value
                 hgvs_status[ws[cell_name_hgvs].value] = 'Secondary'
                 hgvs_vars.append(ws[cell_name_hgvs].value)
-
 # first attempt local conversion
 print('Local Conversion Begun for ' + args.xlsx_file.split(' ')[0])
 local_conversion_results = subprocess.check_output(['/Users/mcgaugheyd/git/casey_to_gemini/src/hgvs_to_vcf.py','--comma', ','.join(hgvs_vars)]).decode('utf-8')
-
 print('VEP Conversion Begun for ' + args.xlsx_file.split(' ')[0])
 # then take the failures and run against VEP
 hgvs_file_name =  args.xlsx_file.split(' ')[0] + '_' + str(time.time()) + '.tmp'
 hgvs_file = open(hgvs_file_name, 'w')
 successful_local_conversion_results = ''
+fail_count = 0
 for line in local_conversion_results.split('\n'):
     if 'ERROR' in line or 'None' in line:
+        fail_count += 1
         failed_hgvs = line.split('\t')[2]
         hgvs_file.write(failed_hgvs + '\n')
     else:
@@ -96,9 +96,13 @@ for line in local_conversion_results.split('\n'):
 hgvs_file.close()
 ############
 # convert to vcf with VEP
-vep_call = '/Applications/ensembl-vep/./vep -i ' + hgvs_file_name + ' --port=3337 --refseq --database --force --pick --vcf --output_file STDOUT --no_stats'
-vep_vcf = subprocess.check_output(vep_call, shell = True)
-vep_vcf = vep_vcf.decode('utf-8')
+if fail_count > 0:
+    vep_call = '/Applications/ensembl-vep/./vep -i ' + hgvs_file_name + ' --port=3337 --refseq --database --force --pick --vcf --output_file STDOUT --no_stats'
+    vep_vcf = subprocess.check_output(vep_call, shell = True)
+    vep_vcf = vep_vcf.decode('utf-8')
+else:
+    print("No variants to convert with VEP!")
+    vep_vcf = ''
 # rm temp file
 #subprocess.call(['rm',hgvs_file_name])
 
@@ -115,6 +119,7 @@ for k,v in hgvs_zygosity.items():
 vcf_file_name = args.xlsx_file.split(' ')[0] + '_' + panel_type +  '.vcf'
 vcf_file = open(vcf_file_name, 'w')
 
+
 for line in vep_vcf.split('\n')[:-1]:
     if line[0:2] == '##':
         vcf_file.write(line)
@@ -122,9 +127,11 @@ for line in vep_vcf.split('\n')[:-1]:
     elif line[0:6] == '#CHROM':
         vcf_file.write('##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">\n##FORMAT=<ID=CT,Number=1,Type=String,Description="HGVS to VCF Converter Tool. VEP is VEP, DM is David McGaughey invitae hgvs conversion">\n##FORMAT=<ID=JC,Number=1,Type=String,Description="MVL/John Chiang variant status. Primary is his likely deleterious variants. Secondary is likely benign">\n')
         vcf_file.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSAMPLE\n')
+	# write variants, with genotype added (0/1, 1/1, etc.)
     else:
         s_line = line.split('\t')
         if line:
+			# if conversion fails, then write to error file
             if not s_line[4] or not s_line[5]:
                 dat_errorfile.write(s_line[2] + '\t' + str(hgvs_status[s_line[2]]) + '\n')	
             elif 'het' in hgvs_zygosity[s_line[2]]:
@@ -133,6 +140,9 @@ for line in vep_vcf.split('\n')[:-1]:
             else:
                 output =  '\t'.join(s_line[0:5]) + '\t' + '.' + '\tPASS\t.\tGT:CT:JC\t' + '1/1:VEP:' + str(hgvs_status[s_line[2]]) + '\n'
                 vcf_file.write(output)
+
+if vep_vcf == '':
+	vcf_file.write('##fileformat=VCFv4.1\n##FILTER=<ID=PASS,Description="All filters passed">\n##VEP="v89" time="2017-07-28 19:09:25" db="homo_sapiens_core_89_37@ensembldb.ensembl.org" ensembl-funcgen=89.678099a ensembl-io=89.984214b ensembl-variation=89.0afbd49 ensembl=89.ce16adf assembly="GRCh37.p13" gencode="GENCODE 19" genebuild="2011-04" refseq="01_2015"\n##INFO=<ID=CSQ,Number=.,Type=String,Description="Consequence annotations from Ensembl VEP. Format: Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|DISTANCE|STRAND|FLAGS|SYMBOL_SOURCE|HGNC_ID|REFSEQ_MATCH">\n##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">\n##FORMAT=<ID=CT,Number=1,Type=String,Description="HGVS to VCF Converter Tool. VEP is VEP, DM is David McGaughey invitae hgvs conversion">\n##FORMAT=<ID=JC,Number=1,Type=String,Description="MVL/John Chiang variant status. Primary is his likely deleterious variants. Secondary is likely benign">\n##INFO=<ID=OLD_VARIANT,Number=.,Type=String,Description="Original chr:pos:ref:alt encoding">\n#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSAMPLE\n')
 # write the locally converted vcf
 for line in local_conversion_results.split('\n')[:-1]:
     s_line = line.split('\t')
