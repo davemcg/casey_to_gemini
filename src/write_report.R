69458428d51bc895a55a879ff6d820b48a80419d
David McGaughey <mcgaughey@gmail.com>
2017-11-01 13:14:10 -0400

    call rmarkdown library, clean up formatting

--- src/write_report.R
+++ src/write_report.R
@@ -1,11 +1,12 @@
-#!/usr/local/apps/R/gcc_6.2.0/3.4.0/bin/Rscript
+#!/usr/local/apps/R/gcc_4.9.1/3.4.0/bin/Rscript
 args = commandArgs(trailingOnly=TRUE)
-
-rmarkdown::render(args[1],
-				  output_file = args[6],
-                  params = list(
-                      master_db = args[2],
-                      patient_vcf = args[3],
-					  failed = args[4],
-					  patient_ID = args[5])
-                  )
+library(rmarkdown)
+render(args[1],
+		output_file = args[6],
+		output_format = 'html_document',
+        params = list(
+        	master_db = args[2],
+            patient_vcf = args[3],
+			failed = args[4],
+			patient_ID = args[5]),
+		encoding = 'utf-8')
